2ba6da1f3da853c63ce0dc24a2e351c05e70511f
D-Mariia <maridegt-ipt27@lll.kpi.ua>
2024-10-15 22:42:29 +0300

    Add files via upload

--- Python-2024-FI-23-Dehtiarova.py
+++ Python-2024-FI-23-Dehtiarova.py
@@ -1,175 +1,209 @@
-import timeit
+import time
 import matplotlib.pyplot as plt
-import random
+
+
+def mtime (func, *args, repeats=10):
+    start = time.time()
+    for _ in range(repeats):
+        result = func(*args)
+    end = time.time()
+    avg_time = (end - start) / repeats
+    return avg_time, result
+
 
 class Operations:
+    def __init__(self, hex_value):
+        self.bin_value = self.hex_to_bin(hex_value)
 
-    def __init__(self, value="0", bit_length=2048, base=16):
-        self.bit_length = bit_length
+    @staticmethod
+    def hex_to_bin(hex_str):
+        return bin(int(hex_str, 16))[2:].zfill(2048)
 
-        if isinstance(value, str):
-            if base == 16:
-                if not all(c in '0123456789ABCDEF' for c in value.upper()):
-                    raise ValueError(f"Некоректно: {value}")
-                self.value = int(value, 16)
-            else:
-                raise ValueError(f"Невідома система числення: {base}")
-        else:
-            self.value = value
+    @staticmethod
+    def bin_to_hex(bin_str):
+        return hex(int(bin_str, 2))[2:].lstrip('0').upper() or '0'
 
-        if self.value >= (1 << bit_length):
-            raise OverflowError(f"Число перевищує {bit_length} бітів!")
-
-    def from_constant(value):
-        if value not in [0, 1]:
-            raise ValueError("Допустимі лише константи 0 і 1.")
-        return Operations(hex(value)[2:], base=16)
+    @staticmethod
+    def small_constant_to_bin(value):
+        if value not in (0, 1):
+            raise ValueError("Можливі тільки константи 0 або 1")
+        return str(value).zfill(2048)
 
     def add(self, other):
-        result = self.value + other.value
-        carry = result >> self.bit_length
-        result &= (1 << self.bit_length) - 1
+        return self.bin_to_hex(self.add_binary(self.bin_value, other.bin_value))
 
-        return Operations(hex(result)[2:], self.bit_length, base=16), carry
+    @staticmethod
+    def add_binary(a, b):
+        max_len = max(len(a), len(b))
+        a = a.zfill(max_len)
+        b = b.zfill(max_len)
 
-    def sub(self, other):
-        if self.value < other.value:
-            raise ValueError("Результат віднімання негативний")
+        result = ''
+        carry = 0
 
-        result = self.value - other.value
-        borrow = 1 if result < 0 else 0
-        result &= (1 << self.bit_length) - 1
+        for i in range(max_len - 1, -1, -1):
+            r = carry
+            r += 1 if a[i] == '1' else 0
+            r += 1 if b[i] == '1' else 0
 
-        return Operations(hex(result)[2:], self.bit_length, base=16), borrow
+            result = ('1' if r % 2 == 1 else '0') + result
+            carry = 0 if r < 2 else 1
 
-    def mul(self, other):
-        if self.value == 0 or other.value == 0:
-            return Operations(0)
+        if carry != 0:
+            result = '1' + result
 
-        result = self.value * other.value
-        result &= (1 << self.bit_length) - 1
+        return result.zfill(2048)
 
-        return Operations(hex(result)[2:], self.bit_length, base=16)
+    @staticmethod
+    def subtract(a, b):
+        comparison = Operations.long_cmp(a, b)
+        if comparison == -1:
+            return "Помилка: неможливо відняти більше число від меншого."
+        return Operations.bin_to_hex(Operations.subtract_binary(a, b))
 
-    def div(self, other):
-        if other.value == 0:
-            raise ZeroDivisionError("Ділення на нуль неможливе.")
+    @staticmethod
+    def subtract_binary(a, b):
+        max_len = max(len(a), len(b))
+        a = a.zfill(max_len)
+        b = b.zfill(max_len)
 
-        q = self.value // other.value
-        r = self.value % other.value
+        result = ''
+        borrow = 0
 
-        return Operations(hex(q)[2:], self.bit_length, base=16), Operations(hex(r)[2:], self.bit_length, base=16)
+        for i in range(max_len - 1, -1, -1):
+            r = int(a[i]) - borrow
+            r -= int(b[i])
 
-    def sq(self):
-        return self.mul(self)
+            if r < 0:
+                r += 2
+                borrow = 1
+            else:
+                borrow = 0
 
-    def __str__(self):
-        return self.to_base(16)
+            result = str(r) + result
 
-    def to_base(self, base):
-        if base == 2:
-            return bin(self.value)[2:] or '0'
-        elif base == 10:
-            return str(self.value)
-        elif base == 16:
-            return hex(self.value)[2:].upper() or '0'
-        else:
-            raise ValueError(f"Невідома система числення: {base}")
+        return result.lstrip('0').zfill(2048)
 
-    def input_number(bit_length=2048):
-        value = input("Введіть число у шістнадцятковій системі числення: ")
-        return Operations(value, bit_length, base=16)
+    @staticmethod
+    def long_mul_one_digit(a, b_digit):
+        carry = 0
+        result = ['0'] * (len(a) + 1)
 
-    def time_operations(a, b):
-        operations = {
-            "Addition": 'a.add(b)',
-            "Subtraction": 'a.sub(b)',
-            "Multiplication": 'a.mul(b)',
-        }
+        for i in range(len(a) - 1, -1, -1):
+            temp = int(a[i]) * b_digit + carry
+            result[i + 1] = str(temp % 2)
+            carry = temp // 2
 
-        if b.value != 0:
-            operations["Division"] = 'a.div(b)'
-        operations["Square"] = 'a.sq()'
+        result[0] = str(carry)
+        return ''.join(result).lstrip('0').zfill(2048)
 
-        times = {}
-        for op, expr in operations.items():
-            timer = timeit.Timer(lambda: eval(expr))
-            execution_time = timer.timeit(number=1000)
-            times[op] = execution_time / 1000
+    @staticmethod
+    def long_mul(a, b):
+        result = '0' * (len(a) + len(b))
 
-        return times
+        for i in range(len(b) - 1, -1, -1):
+            temp = Operations.long_mul_one_digit(a, int(b[i]))
+            temp = Operations.long_shift_digits_to_high(temp, len(b) - 1 - i)
+            result = Operations.add_binary(result, temp)
 
-    def test_identities():
-        a = Operations(hex(random.randint(1, 2 ** 2048 - 1))[2:])
-        b = Operations(hex(random.randint(1, 2 ** 2048 - 1))[2:])
-        c = Operations(hex(random.randint(1, 2 ** 2048 - 1))[2:])
+        return Operations.bin_to_hex(result)
 
-        # Перевірка тотожності 1: (a + b) * c = c * (a + b) = a * c + b * c
-        sum_ab, _ = a.add(b)
-        left_side = sum_ab.mul(c)
-        right_side_1 = c.mul(sum_ab)
-        right_side_2, _ = a.mul(c).add(b.mul(c))
+    @staticmethod
+    def long_shift_digits_to_high(b, n):
+        return b + '0' * n
 
-        print("1:")
-        print(
-            f"Left side: {left_side.to_base(16)},\n Right side 1: {right_side_1.to_base(16)}, \n Right side 2: {right_side_2.to_base(16)}")
-        print("1:", left_side.value == right_side_1.value == right_side_2.value)
+    @staticmethod
+    def long_cmp(a, b):
+        a = a.lstrip('0')
+        b = b.lstrip('0')
 
-        n = random.randint(100, 200)
+        if len(a) > len(b):
+            return 1
+        elif len(a) < len(b):
+            return -1
 
-        # Перевірка тотожності 2: n * a = (a + a + ... + a) / n
-        sum_a = Operations(0)
-        for _ in range(n):
-            sum_a, _ = sum_a.add(a)
+        for i in range(len(a)):
+            if a[i] > b[i]:
+                return 1
+            elif a[i] < b[i]:
+                return -1
 
-        left_side_2 = sum_a
-        right_side_2 = a.mul(Operations(n))
+        return 0
 
-        print("\n 2:")
-        print(f"Left side: {left_side_2.to_base(16)}, \n  Right side: {right_side_2.to_base(16)}")
-        print("2:", left_side_2.value == right_side_2.value)
+    @staticmethod
+    def long_div_mod(a, b):
+        if b == '0':
+            return "Помилка: ділення на нуль."
+        if a == '0':
+            return ('0', '0')
+
+        R = a
+        Q = '0'
+        C = b
+        shift = 0
+
+        while Operations.long_cmp(R, C) >= 0:
+            C = Operations.long_shift_digits_to_high(b, shift)
+            shift += 1
+
+        while shift > 0:
+            shift -= 1
+            C = Operations.long_shift_digits_to_high(b, shift)
+
+            if Operations.long_cmp(R, C) >= 0:
+                R = Operations.subtract_binary(R, C)
+                Q = Operations.add_binary(Q, '1' + '0' * shift)
+
+        return (Operations.bin_to_hex(Q), Operations.bin_to_hex(R))  # Повертаємо частку і залишок
+
+    @staticmethod
+    def input_number(prompt):
+        while True:
+            hex_value = input(prompt)
+            try:
+                int(hex_value, 16)
+                return hex_value
+            except ValueError:
+                print("Невірний формат. Будь ласка, введіть 16-кове число.")
+
+    def square(self):
+        return Operations.long_mul(self.bin_value, self.bin_value)
+
 
 
 if __name__ == "__main__":
-    Operations.test_identities()
+    num1_hex = Operations.input_number("Введіть перше число (16-кове): ")
+    num2_hex = Operations.input_number("Введіть друге число (16-кове): ")
 
-    a = Operations.input_number()
-    b = Operations.input_number()
+    a = Operations(num1_hex)
+    b = Operations(num2_hex)
 
-    sum_result, carry = a.add(b)
-    print(f"Сума: {sum_result.to_base(16)}")
-    print(f"Перенос (carry): {carry}")
+    add_time, add_result = mtime(a.add, b)
+    sub_time, sub_result = mtime(Operations.subtract, a.bin_value, b.bin_value)
+    mul_time, mul_result = mtime(Operations.long_mul, a.bin_value, b.bin_value)
+    div_time, (div_quotient, div_remainder) = mtime(Operations.long_div_mod, a.bin_value, b.bin_value)
+    square_time, square_result = mtime(a.square)
 
-    try:
-        diff_result, borrow = a.sub(b)
-        print(f"Різниця: {diff_result.to_base(16)}")
-        print(f"Запозичення (borrow): {borrow}")
-    except ValueError as e:
-        print(e)
+    print(f"Результат додавання: {add_result}")
+    print(f"Середній час виконання додавання: {add_time:.10f} сек")
 
-    product_result = a.mul(b)
-    print(f"Добуток: {product_result.to_base(16)}")
+    print(f"Результат віднімання: {sub_result}")
+    print(f"Середній час виконання віднімання: {sub_time:.10f} сек")
 
-    sq_result = a.sq()
-    print(f"Квадрат числа A: {sq_result.to_base(16)}")
+    print(f"Результат множення: {mul_result}")
+    print(f"Середній час виконання множення: {mul_time:.10f} сек")
 
-    try:
-        q_result, r_result = a.div(b)
-        print(f"Частка від ділення: {q_result.to_base(16)}")
-        print(f"Остача від ділення: {r_result.to_base(16)}")
-    except ZeroDivisionError as e:
-        print(e)
+    print(f"Частка при діленні: {div_quotient}")
+    print(f"Залишок при діленні: {div_remainder}")
+    print(f"Середній час виконання ділення: {div_time:.10f} сек")
 
-    execution_times = Operations.time_operations(a, b)
+    print(f"Результат піднесення до квадрату: {square_result}")
+    print(f"Середній час виконання піднесення до квадрату: {square_time:.10f} сек")
 
-    for op, time in execution_times.items():
-        print(f"{op}: {time:.10f} seconds")
+    operations = ['Додавання', 'Віднімання', 'Множення', 'Ділення', 'Квадрат']
+    times = [add_time, sub_time, mul_time, div_time, square_time]
 
-    operations = list(execution_times.keys())
-    times = list(execution_times.values())
-
-    plt.bar(operations, times)
-    plt.ylabel('Time (seconds)')
-    plt.title('')
-    plt.xticks(rotation=45)
+    plt.barh(operations, times, color='green')
+    plt.xlabel('Час виконання (сек)')
+    plt.title('Час виконання арифметичних операцій')
     plt.show()
